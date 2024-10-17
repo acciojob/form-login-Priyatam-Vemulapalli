2dd36e110b6128efdd066e094b303512ee37cab2
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-17 20:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	let element=document.getElementById("form1");
+	let fname = element.elements['fname'].values;
+	let lname = element.elements['lname'].values;
+
+	alert(fname +" "+lname);
+	
 
 }
